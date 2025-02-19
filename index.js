80a555bcf8f856661e4b7008f803ba0bc12dc1b8
Voichita Maria <sabo_voichita@yahoo.com>
2025-02-19 18:12:16 +0100

    extract everything from HTML to JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Welcome");
+function createHeader() {
+  return `<header><img src="images/logo.jpg" alt="logo" id="logo"/></header>`;
+}
+
+function createTitle() {
+  return `<h1>Amo Gift</h1>`;
+}
+document.body.innerHTML = createHeader() + createTitle();
